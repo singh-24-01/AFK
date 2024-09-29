900d8bdd86996ef6f49accbbfe8b60613b891bbe
Sandeep Singh <76127610+singh-24-01@users.noreply.github.com>
2024-09-29 20:16:27 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+document.getElementById('planning-link').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('planning-section').style.display = 'block';
+    document.getElementById('mlsd-section').style.display = 'none';
+    document.getElementById('amsd-section').style.display = 'none';
+});
+
+document.getElementById('mlsd-link').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('planning-section').style.display = 'none';
+    document.getElementById('mlsd-section').style.display = 'block';
+    document.getElementById('amsd-section').style.display = 'none';
+});
+
+document.getElementById('amsd-link').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('planning-section').style.display = 'none';
+    document.getElementById('mlsd-section').style.display = 'none';
+    document.getElementById('amsd-section').style.display = 'block';
+});
