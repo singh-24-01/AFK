fcf2f0897b6b1b67cedc71e8a8866fd9c7bb3550
Sandeep Singh <76127610+singh-24-01@users.noreply.github.com>
2024-09-30 01:23:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 document.getElementById('planning-link').addEventListener('click', function(event) {
     event.preventDefault();
-    document.getElementById('planning-section').style.display = 'block';
-    document.getElementById('presentation-section').style.display = 'none';
-    document.getElementById('mlsd-section').style.display = 'none';
-    document.getElementById('amsd-section').style.display = 'none';
+    showSection('planning-section');
 });
 
 document.getElementById('presentation-link').addEventListener('click', function(event) {
     event.preventDefault();
-    document.getElementById('planning-section').style.display = 'none';
-    document.getElementById('presentation-section').style.display = 'block';
-    document.getElementById('mlsd-section').style.display = 'none';
-    document.getElementById('amsd-section').style.display = 'none';
+    showSection('presentation-section');
 });
 
 document.getElementById('mlsd-link').addEventListener('click', function(event) {
     event.preventDefault();
-    document.getElementById('planning-section').style.display = 'none';
-    document.getElementById('presentation-section').style.display = 'none';
-    document.getElementById('mlsd-section').style.display = 'block';
-    document.getElementById('amsd-section').style.display = 'none';
+    showSection('mlsd-section');
 });
 
 document.getElementById('amsd-link').addEventListener('click', function(event) {
     event.preventDefault();
-    document.getElementById('planning-section').style.display = 'none';
-    document.getElementById('presentation-section').style.display = 'none';
-    document.getElementById('mlsd-section').style.display = 'none';
-    document.getElementById('amsd-section').style.display = 'block';
+    showSection('amsd-section');
 });
+
+document.getElementById('plan-link').addEventListener('click', function(event) {
+    event.preventDefault();
+    showSection('plan-section');
+});
+
+// Fonction pour afficher la section souhaitée et masquer les autres
+function showSection(sectionId) {
+    const sections = ['planning-section', 'presentation-section', 'mlsd-section', 'amsd-section', 'plan-section'];
+    sections.forEach(function(id) {
+        document.getElementById(id).style.display = (id === sectionId) ? 'block' : 'none';
+    });
+}
